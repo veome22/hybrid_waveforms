e0212eed18123cd1f4939f191a940358390ab464
Veome Kapil <veomekapil@gmail.com>
2024-01-13 16:11:04 -0500

    Updated all code

--- gwbench_network_funcs.py
+++ gwbench_network_funcs.py
@@ -15,7 +15,7 @@ network_dict = {
 def get_network_spec(net_key):
     return network_dict[net_key]
 
-def get_network_snr(inj_params, f_min=5., f_max=1024., network_key = None, network_spec = ['CE-40_C', 'CE-20_S', 'ET_ET1', 'ET_ET2', 'ET_ET3'], approximant='IMRPhenomXAS', deriv_symbs_string = 'Mc eta DL chi1z chi2z iota ra dec psi', cond_num=1e25, **kwargs):
+def get_network_snr(inj_params, f_min=5., f_max=1024., network_key = None, network_spec = ['CE-40_C', 'CE-20_S', 'ET_ET1', 'ET_ET2', 'ET_ET3'], approximant='IMRPhenomXAS', deriv_symbs_string = 'Mc eta DL chi1z chi2z iota ra dec psi', cond_num=1e25):
 
     # if plain text network key is passed, override full network_spec
     if network_key is not None:
@@ -42,7 +42,7 @@ def get_network_snr(inj_params, f_min=5., f_max=1024., network_key = None, netwo
     net.set_net_vars(
         f=f, inj_params=inj_params,
         deriv_symbs_string=deriv_symbs_string,
-        use_rot=use_rot, **kwargs
+        use_rot=use_rot
         )
 
     # setup antenna patterns, location phase factors, and PSDs
@@ -57,7 +57,7 @@ def get_network_snr(inj_params, f_min=5., f_max=1024., network_key = None, netwo
     return net
 
 
-def get_network_response(inj_params, f_min=5., f_max=1024., d_f = 2**-4,
+def get_network_response(inj_params, f_min=5., f_max=1024., d_f=2**-4,
         network_key = None, 
         network_spec = ['CE-40_C', 'CE-20_S', 'ET_ET1', 'ET_ET2', 'ET_ET3'], 
         approximant='IMRPhenomXAS', 
@@ -68,7 +68,6 @@ def get_network_response(inj_params, f_min=5., f_max=1024., d_f = 2**-4,
     if network_key is not None:
         network_spec = network_dict[network_key]
 
-    print(network_spec)
     # initialize the network with the desired detectors
     net = network.Network(network_spec)
 
@@ -118,20 +117,17 @@ def get_network_response(inj_params, f_min=5., f_max=1024., d_f = 2**-4,
         # covariance matrices, error estimates, and inversion errors
         net.calc_errors(cond_sup=cond_num)
 
-    # calculate the 90%-credible sky area (in deg)
-    # net.calc_sky_area_90()
-
     return net
 
 
 
-def get_hybrid_network_response(inj_params1, f_min=5., f_max=1024., d_f = 2**-4,
-        inj_params2 = None,
+def get_hybrid_network_response(inj_params, #inj_params1=None, 
+        f_min=5., f_max=1024., d_f=2**-4,
         network_key = None,
         network_spec = ['CE-40_C', 'CE-20_S', 'ET_ET1', 'ET_ET2', 'ET_ET3'],
         approximant1='IMRPhenomXAS', approximant2='IMRPhenomD',
         deriv_symbs_string = 'Mc eta chi1z chi2z DL tc phic iota ra dec psi',
-        cond_num=1e25, **kwargs):
+        cond_num=1e25, calc_detector_responses=True, calc_derivs=True, calc_fisher=True, **kwargs):
 
     # if plain text network key is passed, override full network_spec
     if network_key is not None:
@@ -155,33 +151,39 @@ def get_hybrid_network_response(inj_params1, f_min=5., f_max=1024., d_f = 2**-4,
 
     # pass all these variables to the network
     net.set_net_vars(
-        f=f, inj_params=inj_params2,
+        f=f, inj_params=inj_params,
         deriv_symbs_string=deriv_symbs_string,
         use_rot=use_rot, **kwargs
         )
 
     # compute the WF polarizations
     net.calc_wf_polarizations()
-    # compute the WF polarizations and their derivatives
-    net.calc_wf_polarizations_derivs_num()
+
+    if calc_derivs:
+        # compute the WF polarizations and their derivatives
+        net.calc_wf_polarizations_derivs_num()
 
     # setup antenna patterns, location phase factors, and PSDs
     net.setup_ant_pat_lpf_psds()
 
     # compute the detector responses
     net.calc_det_responses()
-    # compute the detector responses and their derivatives
-    net.calc_det_responses_derivs_num()
+
+    if calc_derivs:
+        # compute the detector responses and their derivatives
+        net.calc_det_responses_derivs_num()
 
     # calculate the network and detector SNRs
     net.calc_snrs()
 
-    # calculate the network and detector Fisher matrices, condition numbers,
-    # covariance matrices, error estimates, and inversion errors
-    net.calc_errors(cond_sup=cond_num)
+    if calc_fisher:
+        # calculate the network and detector Fisher matrices, condition numbers,
+        # covariance matrices, error estimates, and inversion errors
+        net.calc_errors(cond_sup=cond_num)
 
-    # compute the cutler-vallisneri bias with respect to the approximant1 waveform model
-    wf_true = wfc.Waveform(wf_model_name = 'lal_bbh', wf_other_var_dic = {'approximant': approximant1})
-    net.calc_cutler_vallisneri_bias(wf=wf_true, inj_params=inj_params1)
+    # # compute the cutler-vallisneri bias with respect to the approximant1 waveform model
+    # wf_true = wfc.Waveform(wf_model_name = 'lal_bbh', wf_other_var_dic = {'approximant': approximant1})
+    # net.calc_cutler_vallisneri_bias(wf=wf_true, inj_params=inj_params1)
 
     return net
+
